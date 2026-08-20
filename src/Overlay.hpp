d51f33ba896e73e262ba8a3203052675e76350c5
reallefroge <contactlefroge@gmail.com>
2026-08-19 23:14:33 -0600

    Update Overlay.hpp

--- src/Overlay.hpp
+++ src/Overlay.hpp
@@ -4,11 +4,44 @@
 #include <QTcpServer>
 #include <QWidget>
 class QTextBrowser; class QLabel;
-class OverlayServer final : public QObject { Q_OBJECT
-public: explicit OverlayServer(QObject* parent=nullptr); bool start(quint16 preferredPort=8080); void stop(); quint16 port()const{return server_.serverPort();} void ingest(const ChatMessage&); void setViewers(const QString&,int); void clear();
-private: void accept(); QByteArray responseFor(const QByteArray&); QTcpServer server_; QList<QPair<quint64,ChatMessage>> messages_; QHash<QString,int> viewers_; quint64 cursor_{}; };
-class PopoutChat final : public QWidget { Q_OBJECT
-public: explicit PopoutChat(QWidget* parent=nullptr);
-public slots: void appendMessage(const ChatMessage&); void showEvent(const StreamEvent&); void setViewers(const QString&,int); void setGhostMode(bool); void setClearBackground(bool); void setOpacityPercent(int);
-private: QTextBrowser* chat_{}; QLabel* event_{}; QLabel* viewers_{}; QHash<QString,int> counts_; };
+class OverlayServer final : public QObject {
+    Q_OBJECT
 
+public:
+    explicit OverlayServer(QObject* parent = nullptr);
+    bool start(quint16 preferredPort = 8080);
+    void stop();
+    quint16 port() const { return server_.serverPort(); }
+    void ingest(const ChatMessage& message);
+    void setViewers(const QString& platform, int count);
+    void clear();
+
+private:
+    void accept();
+    QByteArray responseFor(const QByteArray& target);
+    QTcpServer server_;
+    QList<QPair<quint64, ChatMessage>> messages_;
+    QHash<QString, int> viewers_;
+    quint64 cursor_{};
+};
+
+class PopoutChat final : public QWidget {
+    Q_OBJECT
+
+public:
+    explicit PopoutChat(QWidget* parent = nullptr);
+
+public slots:
+    void appendMessage(const ChatMessage& message);
+    void showEvent(const StreamEvent& event);
+    void setViewers(const QString& platform, int count);
+    void setGhostMode(bool enabled);
+    void setClearBackground(bool enabled);
+    void setOpacityPercent(int percent);
+
+private:
+    QTextBrowser* chat_{};
+    QLabel* event_{};
+    QLabel* viewers_{};
+    QHash<QString, int> counts_;
+};
